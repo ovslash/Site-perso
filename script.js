a436cc76337f9b359b79927609508dc208de3e2e
VILLAVERDE <ovslash@hotmail.fr>
2023-01-19 23:27:53 +0100

    Menu deroulant
    Correction de bug lors du passage en ecran ordi
    a tester

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-var bouton = document.getElementById("btn");
-var nav = document.getElementById("menu_sections");
-var liens = document.getElementsByClassName("menu_sections_lien");
+if (window.screen.width <= 720) {
+  var bouton = document.getElementById("btn");
+  var nav = document.getElementById("menu_sections");
+  var liens = document.getElementsByClassName("menu_sections_lien");
 
-console.log(liens);
-
-bouton.onclick = function (e) {
-  if (nav.style.display == "flex") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "flex";
-  }
-};
-
-for (let lien of liens) {
-  lien.onclick = function (e) {
-    nav.style.display = "none";
-  };
+  document.addEventListener("mouseup", function (event) {
+    for (let lien of liens) {
+      lien.onclick = function (e) {
+        nav.style.display = "none";
+      };
+    }
+    if (bouton.contains(event.target)) {
+      if (nav.style.display == "flex") {
+        nav.style.display = "none";
+      } else {
+        nav.style.display = "flex";
+      }
+    } else if (!nav.contains(event.target)) {
+      nav.style.display = "none";
+      console.log("11111111111111");
+    }
+  });
 }
